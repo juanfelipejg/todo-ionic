06c7ae4cc81ff858279478a0747fa008003c5162
felipe jarmillo <felipe.jaramillo@booktofly.com.co>
2021-12-08 15:26:33 -0500

    Navigate between pages

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { List } from 'src/app/models/list.model';
 import { WishesService } from 'src/app/services/wishes.service';
 
 @Component({
@@ -8,6 +10,13 @@ import { WishesService } from 'src/app/services/wishes.service';
 })
 export class Tab1Page {
 
-  constructor(public wishesService: WishesService) {}
+  constructor(
+    public wishesService: WishesService,
+    private router: Router,
+  ) { }
+
+  public addList(): void {
+    this.router.navigateByUrl('tabs/tab1/add');
+  }
 
 }
--- src/app/services/wishes.service.ts
+++ src/app/services/wishes.service.ts
@@ -9,6 +9,9 @@ export class WishesService {
   lists: List[] = [];
 
   constructor() {
-    console.log('service inicialized');
+    const list1 = new List('Get infinite stocks');
+    const list2 = new List('Get became super sayayin');
+
+    this.lists.push(list1, list2);
   }
 }
